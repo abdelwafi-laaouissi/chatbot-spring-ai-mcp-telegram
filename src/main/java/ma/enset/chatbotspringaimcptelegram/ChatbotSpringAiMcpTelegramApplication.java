3e2010b804cb49216808e03a76de9736d3aadec3
abdelwafi-laaouissi <laaouissi.f@gmail.com>
2025-11-16 15:39:52 +0100

    first commit

---
+++ src/main/java/ma/enset/chatbotspringaimcptelegram/ChatbotSpringAiMcpTelegramApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.chatbotspringaimcptelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotSpringAiMcpTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotSpringAiMcpTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/chatbotspringaimcptelegram/ChatbotSpringAiMcpTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.chatbotspringaimcptelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotSpringAiMcpTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
